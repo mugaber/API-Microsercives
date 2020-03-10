df297084e147089c3866022fbdcdb9580263c66c
MuGaber <mohammedgaber273@gmail.com>
2020-03-10 22:08:21 +0200

    request header parser

--- index.js
+++ index.js
@@ -13,24 +13,13 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-// timestamp microservice
-app.get('/api/timestamp/:date_string?', function(req, res) {
-  const dateString = req.params.date_string
-  let timestamp
-  let error
-
-  if (!dateString) {
-    timestamp = new Date()
-  } else if (dateString.match(/^\d{4}\-\d{2}\-\d{2}$/)) {
-    timestamp = new Date(dateString)
-    res.json({ unix: timestamp.getTime(), utc: timestamp.toUTCString() })
-  } else if (parseInt(dateString)) {
-    timestamp = new Date(parseInt(dateString))
-  } else {
-    res.json({ error: 'Invalid Date' })
-  }
-
-  res.json({ unix: timestamp.getTime(), utc: timestamp.toUTCString() })
+// request header parser
+app.get('/api/whoami', function(req, res) {
+  res.json({
+    ipaddress: req.headers.host,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
 })
 
 app.listen(port, function(req, res) {
